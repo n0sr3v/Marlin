8a1fe966268053c1af76429df5b09815f5be9e21
Fabian Neumann <fabian.neumann@ima-zlw-ifu.rwth-aachen.de>
2019-04-11 18:05:51 +0200

    changed 215 to 214 devision by 2 and cast to int caused problem!

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -880,7 +880,7 @@
 
 // The size of the print bed
 #define X_BED_SIZE 190
-#define Y_BED_SIZE 215
+#define Y_BED_SIZE 214
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
